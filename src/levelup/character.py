75dbf0cf700bc7eb3c8d002d291a1892d4d92c17
7008-75ers <adhikaridipendra@gmail.com>
2023-09-21 15:38:38 +0000

    initialize the character

--- src/levelup/character.py
+++ src/levelup/character.py
@@ -3,3 +3,10 @@ class Character:
 
     def __init__(self, character_name):
         self.name = character_name
+    def initialize_character_xposition_with(self, x_position):
+        self.start_x = 0
+    
+    def initialize_character_yposition_with( self, y_position):
+        self.start_y = 0
+
+    
